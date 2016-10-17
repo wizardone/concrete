8e270a63df01b76556f8d2a6b244053cd40f6c6e
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-10-17 15:32:16 +0300

    try to work on the very basic inheritance logic

--- lib/concrete.rb
+++ lib/concrete.rb
@@ -1,17 +1,39 @@
 require "concrete/version"
+require 'byebug'
 
 module Concrete
+
   def self.included(base)
-    base.extend(ClassMethods)
+    if [Module, Object].include?(base.superclass)
+      base.extend(MainClassMethods)
+    else
+
+    end
   end
 
-  module ClassMethods
+  module MainClassMethods
     def concrete
       @concrete ||= []
     end
 
+    def concrete=(attr)
+      @concrete = attr
+    end
+
     def concrete_attributes(*attr)
-      concrete = attr
+      self.concrete = attr
+
+      instance_eval do
+        self.concrete.each do |prop|
+          define_method("#{prop}=") do |val|
+            instance_variable_set("@#{prop}", val)
+          end
+
+          define_method("#{prop}") do
+            instance_variable_get("@#{prop}")
+          end
+        end
+      end
     end
   end
 end
--- spec/concrete_spec.rb
+++ spec/concrete_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 require 'byebug'
-SomeClass = Class.new do
+MainClass = Class.new do
+  attr_reader :readonly
   include Concrete
 end
+SubClass = Class.new(MainClass)
 
 describe Concrete do
 
-  subject { SomeClass }
+  subject { MainClass }
 
   it 'has a version number' do
     expect(Concrete::VERSION).not_to be nil
@@ -15,4 +17,24 @@ describe Concrete do
   it 'has an empty array of concrete definitions by default' do
     expect(subject.concrete).to eq([])
   end
- end
+
+  it 'assigns concrete attributes to the class' do
+    subject.concrete_attributes(:name, :value, :chep)
+
+    expect(subject.concrete).not_to be_empty
+    expect(subject.concrete).to match_array([:name, :value, :chep])
+  end
+
+  it 'sets the concrete attributes as methods' do
+    subject.concrete_attributes(:some_value)
+
+    expect(subject.new.some_value).to be_nil
+  end
+
+  it 'inherits the concrete attributes' do
+    subject.concrete_attributes(:some_value)
+    sub = SubClass.new
+
+    expect(sub.some_value).to eq(subject.new.some_value)
+  end
+end
