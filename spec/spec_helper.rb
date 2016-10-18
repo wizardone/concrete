8b01cfb04489e52f6bc1a2953d607e3deeed80f1
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-10-18 21:25:36 +0300

    add codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 require 'concrete'
